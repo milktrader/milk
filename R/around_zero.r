da8d9d799c059ddfcf8a015bc4a73146d46a38c2
Milktrader <milktrader@gmail.com>
2012-07-19 10:27:49 -0400

    depend on quantmod and around_zero

--- R/around_zero.r
+++ R/around_zero.r
@@ -1,5 +1,5 @@
 
-around_zero <- function(returns, title="", dn.col='goldenrod4', up.col='goldenrod'){
+around_zero <- function(returns, main="", xlab="", dn.col='goldenrod4', up.col='goldenrod'){
 
 dens  =  density(na.omit(returns))
 x1    =  min(which(dens$x >= 0))
@@ -8,7 +8,7 @@ x3    =  min(which(dens$x >= -1))
 x4    =  max(which(dens$x <  0))
 
 #png("1.png")
-plot(dens, xlab=title, ylab="", main="", yaxt="n")
+plot(dens, xlab=xlab, ylab="", main=main, yaxt="n")
 with(dens, polygon(x=c(x[c(x3,x3:x4,x4)]), y= c(0, y[x3:x4], 0), col=dn.col))
 with(dens, polygon(x=c(x[c(x1,x1:x2,x2)]), y= c(0, y[x1:x2], 0), col=up.col))
 #dev.off()
