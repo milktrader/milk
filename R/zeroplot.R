7150ac1c3496dc7507c965161ee84c7106b0061d
Milktrader <milktrader@gmail.com>
2012-07-23 23:28:16 -0400

    small change to zeroplot

--- R/zeroplot.R
+++ R/zeroplot.R
@@ -1,4 +1,4 @@
-zeroplot <- function(returns, main="", xlab="",  up.col='goldenrod', dn.col='goldenrod4'){
+zeroplot <- function(returns,  xlab="", up.col='goldenrod', dn.col='goldenrod4', main='SPX'){
 
   RET   = na.omit(returns)
   ups   = nrow(RET[RET > 0])
@@ -10,12 +10,12 @@ zeroplot <- function(returns, main="", xlab="",  up.col='goldenrod', dn.col='gol
   x2    =  max(which(dens$x <  1))
   x3    =  min(which(dens$x >= -1))
   x4    =  max(which(dens$x <  0))
-  
+
   #png("1.png")
   #pdf("1.pdf")
-  plot(dens, xlab=xlab, ylab=ylab, main=main, yaxt="n")
+  plot(dens, xlab=xlab, main=main, yaxt="n")
   with(dens, polygon(x=c(x[c(x1,x1:x2,x2)]), y= c(0, y[x1:x2], 0), col=up.col))
   with(dens, polygon(x=c(x[c(x3,x3:x4,x4)]), y= c(0, y[x3:x4], 0), col=dn.col))
-  legend("topleft", inset=.01,  legend=c(ups,downs), fill=c(up.col, dn.col), cex=.8)
+  legend("topleft", inset=.01,  legend=c(ups,downs), fill=c(up.col, dn.col), cex=.8, bty='n')
 
 }
